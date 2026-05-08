346120992f9e1420a117becf9bb43bc3180f9019
Vinicius-EduSenai <vinicius.m.gomes@edu.senai.br>
2026-05-08 11:03:53 -0300

    Ex40 Concluído

---
+++ ex40.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')();
+
+console.log("--- Soma e Verificação ---");
+
+let v1 = Number(prompt("Insira o primeiro valor numérico: "));
+let v2 = Number(prompt("Insira o segundo valor numérico: "));
+let v3 = Number(prompt("Insira o terceiro valor numérico: "));
+
+let temp
+
+temp = v1 + v2 + v3
+
+if(temp>100){
+    console.log(`${v1}`);
+}else{
+
+}
